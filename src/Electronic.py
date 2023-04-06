680865ba9e987da4d332e667a7a37f12681dff16
Muhammad Rizky Sya'ban <mrsyaban99@gmail.com>
2023-04-06 13:16:15 +0700

    feat: initial class

---
+++ src/Electronic.py
@@ -0,0 +1,12 @@
+class Electronic :
+    def __init__(self) :
+        self.nama : String
+        self.rumah_id : String
+        self.voltase : integer
+        self.cnt_room : integer
+        self.listRoom : List of Room
+        self.ConsumptionMonth : integer
+        self.isSimulate : boolean
+        self.circuitBreaker : CircuitBreaker
+
+    
