06c00b682d5f5a037e5b193415bef833541f3f6c
Vieri Fajar Firdaus <16521294@mahasiswa.itb.ac.id>
2023-04-14 16:01:00 +0700

    feat: Room

--- src/Room.py
+++ src/Room.py
@@ -1,12 +1,11 @@
 class Room :
-    def __init__(self) :
-        self.nama : String
-        self.rumah_id : String
-        self.voltase : integer
-        self.cnt_room : integer
-        self.listRoom : List of Room
-        self.ConsumptionMonth : integer
-        self.isSimulate : boolean
-        self.circuitBreaker : CircuitBreaker
+    def __init__(self, nama : str, rumah_id : int, voltase : int, cnt_room : int, isSimulate : bool, id_circuitBreaker : int) :
+        self.nama : str = nama
+        self.rumah_id : int = rumah_id
+        self.voltase : int = voltase
+        self.cnt_room : int = cnt_room
+        self.isSimulate : bool = isSimulate
+        self.id_circuitBreaker : int = id_circuitBreaker
+    
 
     
