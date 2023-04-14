82b0cf3c6b8d9dd96d470a659072396664a450e2
Vieri Fajar Firdaus <16521294@mahasiswa.itb.ac.id>
2023-04-14 22:59:04 +0700

    add method

--- src/models/Room.py
+++ src/models/Room.py
@@ -1,11 +1,100 @@
+import sqlite3
 class Room :
+
+    # CONSTRUCTOR
     def __init__(self, nama : str, rumah_id : int, voltase : int, cnt_room : int, isSimulate : bool, id_circuitBreaker : int) :
+        self.id:int
         self.nama : str = nama
         self.rumah_id : int = rumah_id
-        self.voltase : int = voltase
         self.cnt_room : int = cnt_room
         self.isSimulate : bool = isSimulate
         self.id_circuitBreaker : int = id_circuitBreaker
-    
+
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+            INSERT INTO circuit_breaker(kapasitas_daya) 
+            VALUES ({0})
+            """
+            .format(voltase)
+        )
+
+        curr.execute(
+            """
+            SELECT id
+            FROM circuit_breaker
+            ORDER BY id 
+            LIMIT 1
+            """
+        )
+
+        circuitID=curr.fetchone()[0]
+
+        curr.execute(
+            """
+            INSERT INTO  Ruangan(nama, rumah_id, id_circuit)
+            VALUES ('{0}', {1}, {2})
+            """
+            .format(nama, rumah_id, circuitID)
+        )
+
+        curr.execute(
+            """
+            SELECT id
+            FROM ruangan
+            ORDER BY id 
+            LIMIT 1
+            """
+        )
+
+        self.id=curr.fetchone()[0]
+
+        curr.close()
+        conn.commit()
+        conn.close()
 
     
+    def setRoom(Name:str, ID:int) :
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+            UPDATE Ruangan
+            SET nama = '{0}'
+            WHERE id = {1}
+            """
+            .format(Name, ID)
+        )
+    
+    def addElectricity(self,nama:str, daya:int, voltase:int, waktu_penggunaan ) :
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+            INSERT INTO  alat_listrik(nama, ruangan_id, daya, voltase, waktu_penggunaan)
+            VALUES ('{0}', {1}, {2}, {3}, {4})
+            """
+            .format(nama,self.id, daya, voltase, waktu_penggunaan)
+        )
+    
+    def getRoomById(id:int) :
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+            SELECT *
+            FROM Ruangan
+            WHERE id = {0}
+            """
+            .format(id)
+        )
+
+        data = curr.fetchone()
+        return Room(data[1], data[2], data[3], data[4], data[5], data[6])
+    
+
