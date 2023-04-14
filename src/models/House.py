44cc01cdaa4d51dc6fccb7e6e5d87e27373e4c55
Muhammad Rizky Sya'ban <mrsyaban99@gmail.com>
2023-04-14 17:10:07 +0700

    feat: edit house

--- src/models/House.py
+++ src/models/House.py
@@ -1,27 +1,74 @@
 import sqlite3
+
 class House :
-    def __init__(self) :
-        self.nama : str
-        self.rumah_id : str
-        self.daya : int
-        self.cnt_room : int
-        self.ConsumptionMonth : int
-        self.isSimulate : bool
-        self.idCircuit : int
-    
-    def setHouse(newName, newPower){
-        self.nama = newName
-        self.voltase
-    }
-    House() 
-    House(string,int)
-    setHouse(string,int)
-    addRoom(string)
-    calculateConsumMonth()
-    showConsumMonth()
-    Simulate()
-    deleteRoom()
-    rename()
+    # CONSTRUCTOR
+    def __init__(self, name, power) :
+        self.id:int
+        self.name:str = name 
+        self.power:int = power
+        self.idCircuit:int
+
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+        curr.execute(
+            """
+            INSERT INTO circuit_breaker(kapasitas_daya)
+            VALUE ({0})
+            """
+            .format(power)
+        )
+
+        curr.execute(
+            """
+            SELECT id 
+            FROM circuit_breaker
+            ORDER BY id
+            LIMIT 1
+            """
+        )
+
+        circuitId = curr.fetchall()
+        
+        curr.execute(
+            """
+            INSERT INTO rumah(nama, id_circuit)
+            VALUE ({0}, {1})
+            """
+            .format(name, circuitId)
+        )
+
+        curr.close()
+        conn.close()
+
+
+    # CONSTRUCTOR BY ID
+    def getHouseById(cls, idHouse):
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+                SELECT * 
+                FROM rumah
+                WHERE id={idHouse}
+            """
+        )
+        data = curr.fetchall()
+        houseId, houseName, housePower, circuitId = data
+
+        self = cls.__new__(cls)
+        self.id = houseId 
+        self.name = houseName
+        self.power = housePower
+        self.idCircuit = circuitId
+
+        curr.close()
+        conn.close()      
+
+        return self
+
+    def addRoom():
+        #something
 
 
     
