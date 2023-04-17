e85c997ceda900dc7462f94be4f80d7dc3d6f8cd
SulthanDA28 <13521159@std.stei.itb.ac.id>
2023-04-17 14:38:33 +0700

    fix electronicController

--- src/controller/electronicController.py
+++ src/controller/electronicController.py
@@ -1,19 +1,52 @@
-import models.Room as Room
+import models.Electronic as Electronic
 import sqlite3
 
 class ElectronicController:
-    def addElectricRoom(nama:str, daya:int, voltase:int, waktu_penggunaan,id:int ):
+    def addElectricRoom(id_ruangan: int, nama : str, daya : int, voltase : int, waktu : int, detail : str, isOn : bool = True):
         try:
-            ubah = Room.Room.getRoomById(id)
-            ubah.addElectricity(nama,daya,voltase,waktu_penggunaan)
+            Electronic.Electronic(nama, daya, voltase, waktu, detail, isOn, id_ruangan)
         except:
             print("Gagal menambahkan elektronik")
     def removeElectricRoom(id):
         try:
-            ambil = Room.Room.getRoomById(id)
-            ambil.removeElectricity()
+            conn = sqlite3.connect('db/wirewolf.db')
+            curr = conn.cursor()
+
+            curr.execute(
+                """
+                DELETE FROM elektronik WHERE id = {0}
+                """
+                .format(id)
+            )
+
+            curr.close()
+            conn.close()
         except:
             print("Gagal menghapus elektronik")
+    def ubahNamaElektronik(id,nama):
+        try:
+            ubah = Electronic.Electronic.getElectronicById(id)
+            ubah.setName(nama)
+        except:
+            print("Gagal mengubah nama elektronik")
+    def ubahWattElektronik(id,daya):
+        try:
+            ubah = Electronic.Electronic.getElectronicById(id)
+            ubah.setWatt(daya)
+        except:
+            print("Gagal mengubah watt elektronik")
+    def ubahDetailElektronik(id,detail):
+        try:
+            ubah = Electronic.Electronic.getElectronicById(id)
+            ubah.setDetail(detail)
+        except:
+            print("Gagal mengubah detail elektronik")
+    def ubahVoltaseElektronik(id,voltase):
+        try:
+            ubah = Electronic.Electronic.getElectronicById(id)
+            ubah.setVoltase(voltase)
+        except:
+            print("Gagal mengubah voltase elektronik")
     def getListElectricRoom(id):
         conn = sqlite3.connect('db/wirewolf.db')
         curr = conn.cursor()
