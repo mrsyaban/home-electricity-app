9da181392a8fe62980bb980e31fbe8675b46644e
SulthanDA28 <13521159@std.stei.itb.ac.id>
2023-04-17 13:14:45 +0700

    return listofHouse

--- src/controller/houseController.py
+++ src/controller/houseController.py
@@ -1,4 +1,5 @@
 import models.House as House
+import sqlite3
 class HouseController:
     def add(self,nama : str,power : int):
         try:
@@ -7,22 +8,37 @@ class HouseController:
             print("Gagal menambahkan rumah")
     def removehouse(self,id):
         try:
-            ambil = House.getHouseById(id)
+            ambil = House.House.getHouseById(id)
             ambil.deleteHouse()
         except:
             print("Gagal menghapus rumah")
     def renameHouse(self,id,nama):
         try:
-            ambil = House.getHouseById(id)
+            ambil = House.House.getHouseById(id)
             ambil.editHouseName(nama)
         except:
             print("Gagal mengubah nama rumah")
     def changePower(self,id,power):
         try:
-            ambil = House.getHouseById(id)
+            ambil = House.House.getHouseById(id)
             ambil.editPowerCap(power)
         except:
             print("Gagal mengubah kapasitas daya")
     def getHouse(self,id):
         return House.getHouseById(id)
+    def getAllHouse(self):
+        conn = sqlite3.connect('db/wirewolf.db')
+        curr = conn.cursor()
+
+        curr.execute(
+            """
+            SELECT * FROM rumah
+            """
+            .format(self.id)
+        )
+        ans = curr.fetchall()
+        curr.close()
+        conn.close()
+        return ans
+
     
