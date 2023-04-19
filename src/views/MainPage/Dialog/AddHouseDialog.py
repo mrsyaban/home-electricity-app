524799d9151376264023355fb5e87692af5eedd5
mutawalle <13521065@std.stei.itb.ac.id>
2023-04-19 07:01:24 +0700

    feat: dialog add house

---
+++ src/views/MainPage/Dialog/AddHouseDialog.py
@@ -0,0 +1,119 @@
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QSpinBox
+from controller.houseController import HouseController
+from models.House import House
+from PyQt5 import QtCore, QtGui, QtWidgets
+class AddHouseDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("My Dialog")
+        self.setupUi()
+
+    def setupUi(self):
+        self.setObjectName("Dialog")
+        self.resize(500, 250)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMinimumSize(QtCore.QSize(500, 250))
+        self.setMaximumSize(QtCore.QSize(500, 250))
+        self.setStyleSheet("background-color: rgb(217, 217, 217);")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.frame = QtWidgets.QFrame(self)
+        self.frame.setMaximumSize(QtCore.QSize(16777215, 100))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout.setContentsMargins(60, -1, 60, -1)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2, 0, QtCore.Qt.AlignBottom)
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setStyleSheet("QLineEdit {\n"
+"    background-color: #939393;\n"
+"    border-radius: 13px;\n"
+"    padding: 5px;\n"
+"    padding-left: 15px;\n"
+"    padding-right: 15px;\n"
+"}\n"
+"\n"
+"QLineEdit:hover {\n"
+"    border: 1px solid #444444;\n"
+"}")
+        self.lineEdit.setPlaceholderText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.verticalLayout_2.addWidget(self.frame)
+        self.frame_3 = QtWidgets.QFrame(self)
+        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_3.setObjectName("frame_3")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.frame_3)
+        self.verticalLayout_3.setContentsMargins(60, -1, 60, -1)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.label_3 = QtWidgets.QLabel(self.frame_3)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_3.addWidget(self.label_3, 0, QtCore.Qt.AlignBottom)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame_3)
+        self.lineEdit_2.setStyleSheet("QLineEdit {\n"
+"    background-color: #939393;\n"
+"    border-radius: 13px;\n"
+"    padding: 5px;\n"
+"    padding-left: 15px;\n"
+"    padding-right: 15px;\n"
+"}\n"
+"\n"
+"QLineEdit:hover {\n"
+"    border: 1px solid #444444;\n"
+"}")
+        self.lineEdit_2.setPlaceholderText("")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout_3.addWidget(self.lineEdit_2)
+        self.verticalLayout_2.addWidget(self.frame_3, 0, QtCore.Qt.AlignTop)
+        self.frame_2 = QtWidgets.QFrame(self)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.frame_2)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton.clicked.connect(self.handle_submit)
+        font = QtGui.QFont()
+        font.setFamily("MS Shell Dlg 2")
+        font.setPointSize(8)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: #444444;\n"
+"font: 75 8pt \"MS Shell Dlg 2\";\n"
+"border-radius: 11px;\n"
+"padding: 5px;\n"
+"padding-left: 15px;\n"
+"padding-right: 15px;\n"
+"color: rgb(217, 217, 217);")
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton, 0, QtCore.Qt.AlignHCenter)
+        self.verticalLayout_2.addWidget(self.frame_2)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label_2.setText(_translate("Dialog", "Nama Rumah"))
+        self.label_3.setText(_translate("Dialog", "Kapasitas Daya (Watt)"))
+        self.pushButton.setText(_translate("Dialog", "Tambahkan"))
+
+
+    def handle_submit(self):
+        name = self.lineEdit.text()
+        power = self.lineEdit_2.text()
+        hasil = int(power)
+        x = House(name, hasil)
+        self.accept()
